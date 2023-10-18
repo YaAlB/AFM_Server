657cfb3115db2e654b4b00be1be74a3ba5b7d155
YaAlB <yassinbehery@gmail.com>
2023-10-18 13:48:28 +1100

    add registration functionality and cleanup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,19 +1,21 @@
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const generateToken = require('../Utils/generateToken')
 const asyncHandler = require('express-async-handler')
 
 // @desc Login
 // @route POST /auth
 // @access Public
 const login = asyncHandler(async (req, res) => {
-    const { username, password } = req.body
+    const { email, password } = req.body
 
-    if (!username || !password) {
+    if (!email || !password) {
+        console.log(email, password)
         return res.status(400).json({ message: 'All fields are required' })
     }
 
-    const foundUser = await User.findOne({ username }).exec()
+    const foundUser = await User.findOne({ email }).exec()
 
     if (!foundUser || !foundUser.active) {
         return res.status(401).json({ message: 'Unauthorized' })
@@ -23,41 +25,53 @@ const login = asyncHandler(async (req, res) => {
 
     if (!match) return res.status(401).json({ message: 'Unauthorized' })
 
-    const accessToken = jwt.sign(
-        {
-            "UserInfo": {
-                "username": foundUser.username,
-                "roles": foundUser.roles
-            }
-        },
-        process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn: '15m' }
-    )
+    const accessToken = generateToken(res, foundUser);
 
-    const refreshToken = jwt.sign(
-        { "username": foundUser.username },
-        process.env.REFRESH_TOKEN_SECRET,
-        { expiresIn: '7d' }
-    )
-
-    // Create secure cookie with refresh token 
-    res.cookie('jwt', refreshToken, {
-        httpOnly: true, //accessible only by web server 
-        secure: true, //https
-        sameSite: 'None', //cross-site cookie 
-        maxAge: 7 * 24 * 60 * 60 * 1000 //cookie expiry: set to match rT
-    })
-
-    // Send accessToken containing username and roles 
+    // Send accessToken containing id and username 
     res.json({ accessToken })
 })
+
+// @desc    Register a new user
+// @route   POST /auth/register
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+    const { username, email, password } = req.body;
+  
+    // Confirm data
+    if (!username || !email || !password) {
+        return res.status(400).json({ message: 'All fields are required' })
+    }
+
+    // Check for duplicate username
+    const duplicate = await User.findOne({ email }).lean().exec()
+
+    if (duplicate) {
+        return res.status(401).json({ message: 'User already exists' })
+    }
+
+    // Hash password 
+    const hashedPwd = await bcrypt.hash(password, 10) // salt rounds
+
+    const userObject = { username, email, "password": hashedPwd }
+  
+    // Create and store new user 
+    const user = await User.create(userObject)
+  
+    if (user) {// created
+        const accessToken = generateToken(res, user._id);
+  
+      res.status(201).json({ message: `New user ${username} created`, accessToken })
+    } else {
+        res.status(400).json({ message: 'Invalid user data received' })
+    }
+  });
 
 // @desc Refresh
 // @route GET /auth/refresh
 // @access Public - because access token has expired
 const refresh = (req, res) => {
     const cookies = req.cookies
-
+    console.log('cookies: ', cookies)
     if (!cookies?.jwt) return res.status(401).json({ message: 'Unauthorized' })
 
     const refreshToken = cookies.jwt
@@ -75,8 +89,7 @@ const refresh = (req, res) => {
             const accessToken = jwt.sign(
                 {
                     "UserInfo": {
-                        "username": foundUser.username,
-                        "roles": foundUser.roles
+                        "id": foundUser._id
                     }
                 },
                 process.env.ACCESS_TOKEN_SECRET,
@@ -100,6 +113,7 @@ const logout = (req, res) => {
 
 module.exports = {
     login,
+    registerUser,
     refresh,
     logout
 }
